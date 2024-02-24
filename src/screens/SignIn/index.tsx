dbf6215e246b1d3f235bf552e0358aca2fcc6a2f
Lucas Papine <lpapine@envsense.com.br>
2024-02-24 16:03:36 -0300

    !

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,16 +1,47 @@
-import { VStack, Image } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 
+import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
-    <VStack flex="1" bg="gray.700">
-      <Image 
+    <VStack flex="1" bg="gray.700" px={5}>
+      <Image
         source={BackgroundImg}
         alt="Pessoas treinando em uma bicicleta."
         resizeMode="contain"
         position="absolute"
       />
+      <Center my={24}>
+        <LogoSvg />
+        <Text color="gray.100" fontSize="sm">
+          Treine sua emnte e seu corpo
+        </Text>
+      </Center>
+
+      <Heading
+        color="gray.100"
+        fontSize="xl"
+        mb="6"
+        fontFamily="heading"
+        textAlign="center"
+      >
+        Acessa a conta
+      </Heading>
+
+      <Center>
+        <Input 
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+
+        <Input 
+          placeholder="Senha" 
+          secureTextEntry
+        />
+      </Center>
     </VStack>
   );
 }
